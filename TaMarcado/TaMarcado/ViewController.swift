73bfc099af1881922a6c5c2c123b1967626edd94
ricardohochman <ricardohochman@hotmail.com>
2015-06-11 12:04:45 -0300

    projeto criado

---
+++ TaMarcado/TaMarcado/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaMarcado
+//
+//  Created by Ricardo Hochman on 11/06/15.
+//  Copyright (c) 2015 Ricardo Hochman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
